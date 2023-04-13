d2ade6dd8185f65368d24fcc560e2f828b898e08
novakova <lenka.novakova020@gmail.com>
2023-04-13 13:41:17 +0200

    link to BE

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
 
   const getApiData = () => {
     fetch(
-      "https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v2/rates/exchangerates?web-api-key=c52a0682-4806-4903-828f-6cc66508329e"
+      "http://localhost:8080/usedb?use=false"
     )
       .then((response) => response.json())
       .then((data) => {
