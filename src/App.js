21517b344cf8a004c5a9c0458d6a6373280bf375
novakova <lenka.novakova020@gmail.com>
2023-04-13 16:19:05 +0200

    FE finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
 import "./App.css";
 import { useState, useEffect } from "react";
 
+function RenderDetail(found) {
+  return (
+    <>
+      {/* <p>{JSON.stringify(found)}</p> */}
+
+      <h5>{found.found.country} - {found.found.shortName} - {found.found.name}</h5>
+      <table class="table ">
+        <thead>
+          <tr>
+            <th scope="col">name</th>
+            <th scope="col">amount</th>
+            <th scope="col">valBuy</th>
+            <th scope="col">valSell</th>
+            <th scope="col">valMid</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{found.found.name}</td>
+            <td>{found.found.amount}</td>
+            <td>{found.found.valBuy}</td>
+            <td>{found.found.valSell}</td>
+            <td>{found.found.valMid}</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="table ">
+        <thead>
+          <tr>
+            <th scope="col">currBuy</th>
+            <th scope="col">currSell</th>
+            <th scope="col">currMid</th>
+            <th scope="col">cnbMid</th>
+            <th scope="col">ecbMid</th>
+            <th scope="col">move</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{found.found.currBuy}</td>
+            <td>{found.found.currSell}</td>
+            <td>{found.found.currMid}</td>
+            <td>{found.found.cnbMid}</td>
+            <td>{found.found.ecbMid}</td>
+            <td>{found.found.move}</td>
+          </tr>
+        </tbody>
+      </table>
+      <h6>version: {found.found.version}</h6>
+    </>
+  );
+}
+
 export default function App() {
   const [currency, setCurrency] = useState([]);
-
-  // const renderDetail = (foundData) => {
-  //    return foundData.name
-  // }
+  const [detail, setDetail] = useState(null);
+  console.log(typeof currency);
 
   const handleClick = (event) => {
     const index = event.currentTarget.id;
     const found = currency[index]; // nalezena korespondujici data k dane mene, ty chci pote zobrazit do druheho okna jako detail meny
-    document.getElementById("detail").innerHTML = JSON.stringify(found);
+    setDetail(found);
   };
 
   const getApiData = () => {
-    fetch(
-      "http://localhost:8080/usedb?use=false"
-    )
+    fetch("http://localhost:8080/usedb?use=true")
       .then((response) => response.json())
       .then((data) => {
         setCurrency(data);
@@ -55,8 +104,14 @@ export default function App() {
             </table>
           </div>
 
-          <div id="window" class="col-md-5 text-break">
-            <p class="text-justify" id="detail">click for detail</p>
+          <div id="window" class="col-md-6 text-break">
+            <p class="text-justify" id="detail">
+              {detail !== null ? (
+                <RenderDetail found={detail} />
+              ) : (
+                "Click for detail"
+              )}
+            </p>
           </div>
         </div>
       </div>
